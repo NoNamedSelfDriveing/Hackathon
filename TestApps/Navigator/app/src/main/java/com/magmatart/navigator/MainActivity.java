14c3747a1f02b8b91b140b4712269a37f27297ea
Soomin Lee <magmatart@gmail.com>
2017-09-01 10:51:44 +0900

    Navigator make complete

--- TestApps/Navigator/app/src/main/java/com/magmatart/navigator/MainActivity.java
+++ TestApps/Navigator/app/src/main/java/com/magmatart/navigator/MainActivity.java
@@ -15,12 +15,13 @@ public class MainActivity extends Activity {
     public static final String TAG = "OverlayActivity";
 
     private double mCoordX = 0;             //현재 X 좌표
-    private double mCoordY = 600;           //현재 Y 좌표
+    private double mCoordY = 500;           //현재 Y 좌표
     private final double tCoordX = 600;     //Target X 좌표
     private final double tCoordY = 400;     //Target Y 좌표
     ImageView imageToRotate;                //네비게이터 이미지
 
-    int degree;                             //좌표
+    int degree = 0;                             //좌표
+    int pDegree = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,15 +52,33 @@ public class MainActivity extends Activity {
                     mCoordX += 10;
 
                     //각도 계산
+                    pDegree = degree;
                     degree = (int)Math.toDegrees(Math.acos( (Math.abs(tCoordY-mCoordY) / (Math.sqrt( (Math.abs(tCoordX-mCoordX)) * (Math.abs(tCoordX-mCoordX)) + (Math.abs(tCoordY-mCoordY)) * (Math.abs(tCoordY-mCoordY)) )))));
+                    //degree = 90 - degree;
+                    //degree += 270;
 
                     //현재 위치 기준 사분면 처리. 확정되지 않아 일단 아무 처리도 하지 않음
-                    if(tCoordX-mCoordX >= 0 && tCoordY-mCoordY >= 0);
-                    else if(tCoordX-mCoordX >= 0 && tCoordY-mCoordY <= 0);
-                    else if(tCoordX-mCoordX <= 0 && tCoordY-mCoordY <= 0);
-                    else if(tCoordX-mCoordX <= 0 && tCoordY-mCoordY >= 0);
+                    if(tCoordX-mCoordX >= 0 && tCoordY-mCoordY >= 0) ;
+                    else if((tCoordX==mCoordX) && (mCoordY>tCoordY)) degree = 180;
+                    else if(tCoordX-mCoordX >= 0 && tCoordY-mCoordY <= 0) degree  = (90-degree) + 90;
+                    else if(tCoordX-mCoordX <= 0 && tCoordY-mCoordY <= 0) degree += 180;
+                    else if(tCoordX-mCoordX <= 0 && tCoordY-mCoordY >= 0) degree  = (90-degree) + 270;
 
                     Log.d(TAG, ""+degree);
+
+                    /*
+                    if(pDegree < 90)
+                        degree = 90 - degree;
+                    if(pDegree >= 90)
+                        degree += 90;
+                    if(pDegree >= 180)
+                        degree += 180;
+                    if(pDegree >= 270)
+                        degree += 270;
+                    if(pDegree >= 360)
+                        degree -= 360;
+                        */
+
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
